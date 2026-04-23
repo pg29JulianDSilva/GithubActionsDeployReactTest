1bda547ccd8e7fe90e5c6680b12daddea67f9018
pg29JulianDSilva <pg29julians@vfs.com>
2026-04-23 11:54:26 -0700

    NullTest01

--- .github/workflows/CommitAndDeploy.cmd
+++ .github/workflows/CommitAndDeploy.cmd
@@ -1,23 +1,26 @@
 echo off
 
 cd..
-
 cd..
+
+echo starting commit and deploy process...
 
 git init
 
-for /f "tokens=*" %%i in ('git status') do set "status=%%i"
-
-if "%status%"=="nothing to commit, working tree clean" (
-	echo "No changes to commit."
-	exit /b
-) else (
-	echo "Changes detected. Proceeding with commit and deploy."
-)
+git fetch origin main
 
 git pull origin main
 
 git add .
+
+if %ERRORLEVEL% NEQ 0 (
+    echo There was an error, please check the error and remember:
+    echo - Ensure you have the correct permissions.
+    echo - Ensure you have saved all the changes before running this script.
+    echo - Ensure you have a stable internet connection.
+    pause
+    exit /b
+)
 
 set /p commitName="Enter commit message:"
 
@@ -27,6 +30,6 @@ git push origin main
 
 npm run deploy
  
-echo "Done"
-pause "Press any key to continue..."
+echo Done
+pause
 exit/b
